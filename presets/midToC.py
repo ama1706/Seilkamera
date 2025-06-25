5c8aa26977b12f822bdef7ffa1b576a3a195ec22
ama1706 <amadeo2030@hotmail.com>
2025-06-25 22:07:35 +0200

    delete preset folder

--- presets/midToC.py
+++
@@ -1,7 +0,0 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).resolve().parent.parent))
-from preset_runner import run_preset
-
-if __name__ == '__main__':
-    run_preset('midToC')
